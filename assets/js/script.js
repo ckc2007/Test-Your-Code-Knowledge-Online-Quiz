61f5aeba4201a6f5ed15be6d74213ccde6ba4964
ckc2007 <ckc2007@gmail.com>
2023-03-19 15:30:31 -0400

    making quiz buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,31 @@ startClick.addEventListener("click", function () {
   // then start the timer countdown...
   setTime();
 });
+
+// create the quiz questons that will display
+// i would make them part of a container and just change out the display of the container...
+// we need to use dom to update conent - create, append, set attributes etc
+var h1El = document.querySelector("h1");
+var instructionsEl = document.querySelector("#instructions");
+var quizEl = document.querySelector("#quiz");
+// make the buttons
+var choice1El = document.createElement("button");
+choice1El.id = "choice-1";
+var choice2El = document.createElement("button");
+choice2El.id = "choice-2";
+var choice3El = document.createElement("button");
+choice3El.id = "choice-3";
+var choice4El = document.createElement("button");
+choice4El.id = "choice-4";
+
+function question1() {
+  h1El.textContent = "Question 1:.......";
+  // how do you hide an element again? none vs visible?
+  instructionsEl.display = "none";
+  //   hide the start button
+  startClick.display = "none";
+  // create and display the four buttons you will use to answer
+  quizEl.
+}
+
+// should i make the question into an object?
