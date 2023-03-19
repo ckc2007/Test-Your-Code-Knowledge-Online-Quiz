4d06581d526b14585e0045192ee8604414e00976
ckc2007 <ckc2007@gmail.com>
2023-03-19 15:08:59 -0400

    got timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // this element selects the time id
-var timeEl = document.getELementById("time");
+var timeEl = document.querySelector("#time");
 
 // this is our start time
 var secondsLeft = 60;
 
 // make an element that listens for a click on the start button
-var startClick = document.getElementById("start-button");
+var startClick = document.querySelector("#start-button");
 
 // we may need a container so we can change the overall content here queston to question....
 // var container = document.querySelector(".container");
@@ -13,13 +13,15 @@ var startClick = document.getElementById("start-button");
 function setTime() {
   var timeInt = setInterval(function () {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + "s";
+    timeEl.textContent = "Time: " + secondsLeft;
     //TO DO make conditions here or a function that can be colled on condition if the answer is WRONG it must deduct x from time...
     // function(){}
 
     if (secondsLeft === 0) {
       // stop the timer at this interval
       clearInterval(timeInt);
+      //   reset time to 60s
+      // secondsLeft = 60
       // TO DO call a function to make the page change here...
       //   function() {}
     }
