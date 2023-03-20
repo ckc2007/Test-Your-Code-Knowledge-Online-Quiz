46222712fec0cf18e88b302c1433438087fa7c2f
ckc2007 <ckc2007@gmail.com>
2023-03-20 13:17:14 -0400

    writing the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var startButton = document.getElementById("start-btn");
 var timeEl = document.getElementById("time");
-var secondsLeft = 100;
+var secondsLeft;
 var highScore = 0;
 var timeInt;
 var buttonContainerEl = document.querySelector("#button-container");
@@ -8,6 +8,8 @@ var questionsAnswered = 0;
 var questions = [];
 
 function setTime() {
+  // lollllllll
+  secondsLeft = 101;
   timeInt = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
@@ -19,10 +21,13 @@ function setTime() {
   }, 1000);
 }
 
+// event handler for start button click
 startButton.addEventListener("click", function () {
+  setNextQuestion();
   setTime();
 });
 
+// event handler for answer button clicks
 buttonContainerEl.addEventListener("click", function (event) {
   var element = event.target;
   if (element.matches("button")) {
@@ -46,3 +51,42 @@ function quizTimeOut() {
 }
 
 function setNextQuestion() {}
+
+// create the questions
+const questions = [
+  {
+    question: "Which is NOT a primitive data type?",
+    answers: [
+      { text: "string", correct: false },
+      { text: "boolean", correct: false },
+      { text: "number", correct: false },
+      { text: "object", correct: true },
+    ],
+  },
+  {
+    question: "Which variable is declared in the global scope?",
+    answers: [
+      { text: "var", correct: true },
+      { text: "let", correct: false },
+      { text: "const", correct: false },
+      { text: "mkdir", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which array method does NOT modify the original array in JavaScript?",
+    answers: [
+      { text: "slice()", correct: true },
+      { text: "reverse()", correct: false },
+      { text: "unshift()", correct: false },
+      { text: "splice()", correct: false },
+    ],
+  },
+  {
+    question: "What is 4 * 2?",
+    answers: [
+      { text: "6", correct: false },
+      { text: "8", correct: true },
+    ],
+  },
+];
