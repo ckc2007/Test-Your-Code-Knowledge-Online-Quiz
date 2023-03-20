3c400545f80d925a98460027fc91a4c18ae83484
ckc2007 <ckc2007@gmail.com>
2023-03-20 11:51:57 -0400

    made setTime function

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById("start-btn");
+var timeEl = document.getElementById("time");
+var secondsLeft = 100;
+var highScore = 0;
+
+function setTime() {
+  var timeInt = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timeInt);
+    }
+  }, 1000);
+}
